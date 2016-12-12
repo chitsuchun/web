fef84915587ab82d6a7101eba8af6b02b19533f7
chitsuchun <chitsuchun@gmail.com>
2016-12-12 20:41:36 +0800

    s

--- hw20161110/js/index.js
+++ hw20161110/js/index.js
@@ -1,58 +1,61 @@
-(function ($){
-    $.fn.MainDataInIt = function () {
-    }
-    $.fn.ComCss = function (property) {
-        var _self = $(this);
-            _self['propObj'] = {};
-        for(x in property){ 
-            _self.propObj['-webkit-'+x] = property[x];
-            _self.propObj['-moz-'+x] = property[x];
-            _self.propObj['-ms-'+x] = property[x];
-            _self.propObj[x] = property[x];
-        }
-        _self.css(_self.propObj);
-        /*for(x in _self.propObj){ 
-            delete _self.propObj[x];
-        }*/
-        delete _self.propObj;
-        property = null;
-        _self = null;      
-    }
-})(jQuery);
+// (function ($){
+//     $.fn.MainDataInIt = function () {
+//     }
+//     $.fn.ComCss = function (property) {
+//         var _self = $(this);
+//             _self['propObj'] = {};
+//         for(x in property){ 
+//             _self.propObj['-webkit-'+x] = property[x];
+//             _self.propObj['-moz-'+x] = property[x];
+//             _self.propObj['-ms-'+x] = property[x];
+//             _self.propObj[x] = property[x];
+//         }
+//         _self.css(_self.propObj);
+//         /*for(x in _self.propObj){ 
+//             delete _self.propObj[x];
+//         }*/
+//         delete _self.propObj;
+//         property = null;
+//         _self = null;      
+//     }
+// })(jQuery);
+
+// $(function(){
+//     $.Body = $('body');
+//     $.Window = $(window);
+//     $.Loading = $.Body.find('div#loading');
+//     $.Window.load(
+//         function(){
+//             $.Loading.velocity(
+//                 {
+//                     'opacity': 0
+//                 }
+//                 , 
+//                 {
+//                     /* Velocity's default options */
+//                     duration: 400,
+//                     easing: "swing",
+//                     queue: "",
+//                     begin: undefined,
+//                     progress: undefined,
+//                     complete: function(){$.Loading.css({'display':'none'}); $.Body.MainDataInIt();},
+//                     display: undefined,
+//                     visibility: undefined,
+//                     loop: false,
+//                     delay: false,
+//                     mobileHA: true
+//                 }
+//             );
+//         }
+//     );
+// });
 
 $(function(){
-    $.Body =$('body');
-    $.Window = $(window);
-    $.Loading = $.Body.find('div#loading');
-    $.Window.load(
-        function(){
-            $.Loading.velocity(
-                {
-                    'opacity': 0
-                }
-                , 
-                {
-                    /* Velocity's default options */
-                    duration: 400,
-                    easing: "swing",
-                    queue: "",
-                    begin: undefined,
-                    progress: undefined,
-                    complete: function(){$.Loading.css({'display':'none'}); $.Body.MainDataInIt();},
-                    display: undefined,
-                    visibility: undefined,
-                    loop: false,
-                    delay: false,
-                    mobileHA: true
-                }
-            );
-        }
-    );
+    alert('ok');
 });
 
 
-
-
+// $( document ).ready( function(){   } )
 
 
 
@@ -113,13 +116,13 @@ eat(lunch);
 
 // document.getElementById('slider_1') = $('#slider').get(0)
 
-console.log($('#slider_1').attr('id'));
+// console.log($('#a,#b,#c').attr('id'));
 
 // <div id='slider'></div>     
 
 
 
-
+//           document.queryselector('#a,#b,#c')
 console.log( document.getElementById('slider_1').getAttribute('id') );
             // labu  .    history        ()     .   laugh      ()
 function lol(){
